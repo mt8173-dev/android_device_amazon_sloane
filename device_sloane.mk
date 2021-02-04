b75544e70c3a71654dc965c04b24d1ddfd527501
Lajos Molnar <lajos@google.com>
2021-02-04 09:46:24 +0100

    sloane: device.mk: TV codecs have been separated, use them

--- device_sloane.mk
+++ device_sloane.mk
@@ -45,6 +45,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_tv.xml:system/etc/media_codecs_google_tv.xml
 
 # Prebuilt Keylayouts
 PRODUCT_COPY_FILES += \
