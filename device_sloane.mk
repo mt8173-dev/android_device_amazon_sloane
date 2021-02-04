76c41d927b98f78eeda360982b00e1895693dc1e
R0rt1z2 <rogerortizleal@gmail.com>
2021-02-04 12:03:36 +0100

    sloane: copy thermal configs for FireTV 2
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- device_sloane.mk
+++ device_sloane.mk
@@ -132,8 +132,6 @@ PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/init/audioserver.rc:system/etc/init/
 # Thermal
 PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/configs/thermal/.ht120.mtc:system/etc/.tp/.ht120.mtc \
-     $(LOCAL_PATH)/configs/thermal/thermal.conf:system/etc/.tp/thermal.conf \
-     $(LOCAL_PATH)/configs/thermal/thermal.off.conf:system/etc/.tp/thermal.off.conf \
      $(LOCAL_PATH)/configs/thermal/thermal.policy.conf:system/etc/.tp/thermal.policy.conf
 
 # Ramdisk
