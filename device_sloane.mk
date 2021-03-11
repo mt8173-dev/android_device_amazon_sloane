0e37a0192edfe84bbcdcba9d43e3d52cbf1bf38e
R0rt1z2 <rogerortizleal@gmail.com>
2021-03-11 19:21:32 +0100

    sloane: copy media_codecs_google_video from fw/av
    * W MediaCodecList: unable to open media codecs configuration xml file: /etc/media_codecs_google_video.xml
    
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- device_sloane.mk
+++ device_sloane.mk
@@ -48,7 +48,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_tv.xml:system/etc/media_codecs_google_tv.xml
+	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_tv.xml:system/etc/media_codecs_google_tv.xml \
+	
 
 # Prebuilt Keylayouts
 PRODUCT_COPY_FILES += \
