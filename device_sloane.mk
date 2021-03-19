1bd1ba9b8fb49db5a2247ff6b1a330b222a1ed86
chrmhoffmann <chrmhoffmann@gmail.com>
2021-03-19 23:47:32 +0100

    sloane: build keymaster HIDL HAL interfaces

--- device_sloane.mk
+++ device_sloane.mk
@@ -46,6 +46,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	android.hardware.power@1.0-impl
 
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl
+
 # Blisrc
 PRODUCT_PACKAGES += \
     libblisrc \
