b192ff0f7af4236083f14d044fb8d82036f9463f
wuxianlin <wuxianlinwxl@gmail.com>
2021-03-20 10:46:32 +0100

    sloane: sepolicy: squash update for android P

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -150,7 +150,19 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/virtual/android_usb/android0/f_
 
 # SELinux
 BOARD_SEPOLICY_DIRS += \
-    $(DEVICE_FOLDER)/sepolicy
+	$(DEVICE_FOLDER)/sepolicy/mediatek/basic/non_plat \
+	$(DEVICE_FOLDER)/sepolicy/mediatek/bsp/non_plat \
+	$(DEVICE_FOLDER)/sepolicy/mt8173/basic \
+	$(DEVICE_FOLDER)/sepolicy/mt8173/bsp \
+	$(DEVICE_FOLDER)/sepolicy
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+	$(DEVICE_FOLDER)/sepolicy/mediatek/basic/plat_public \
+	$(DEVICE_FOLDER)/sepolicy/mediatek/bsp/plat_public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+	$(DEVICE_FOLDER)/sepolicy/mediatek/basic/plat_private \
+	$(DEVICE_FOLDER)/sepolicy/mediatek/bsp/plat_private
 
 # Media Extractors
 BOARD_SECCOMP_POLICY := \
