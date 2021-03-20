4810b7c641b6e30572ceb7f7dec628956e236b9e
Rashed Abdel-Tawab <rashed@linux.com>
2021-03-20 11:21:17 +0100

    sloane: set shipping API level via inherited makefile

--- lineage.mk
+++ lineage.mk
@@ -5,7 +5,7 @@ PRODUCT_RELEASE_NAME := AFTVS
 $(call inherit-product-if-exists, vendor/cm/config/common_full_tv.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l_mr1.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/amazon/sloane/device_sloane.mk)
