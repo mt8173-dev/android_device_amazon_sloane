bbc2955210423740a5149dc98517c5ed9dbedd95
Anton Hansson <hansson@google.com>
2021-03-20 12:10:17 +0100

    sloane: apps: set LOCAL_SDK_VERSION where possible

--- apps/CMLeanbackCustomizer/Android.mk
+++ apps/CMLeanbackCustomizer/Android.mk
@@ -6,5 +6,6 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := CMLeanbackCustomizer
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PRIVATE_PLATFORM_APIS := true
 
 include $(BUILD_PACKAGE)
