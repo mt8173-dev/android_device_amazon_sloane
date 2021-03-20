e11bbf14af6bb43e199a4eb374414fb157170192
jmpfbmx <42691741+jmpfbmx@users.noreply.github.com>
2021-03-20 11:16:54 +0100

    sloane: don't copy media_codecs_performance from fw/av
     * Since it doesn't exists

--- device_sloane.mk
+++ device_sloane.mk
@@ -65,8 +65,7 @@ PRODUCT_COPY_FILES += \
 	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-	frameworks/av/media/libstagefright/data/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
-	frameworks/av/media/libstagefright/data/media_codecs_google_tv.xml:system/etc/media_codecs_google_tv.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_tv.xml:system/etc/media_codecs_google_tv.xml
 
 # Prebuilt Keylayouts
 PRODUCT_COPY_FILES += \
