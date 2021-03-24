7ec9b3df41658bf582f2bc95c0178f277dfc22cb
Stricted <info@stricted.net>
2021-03-24 17:17:17 +0100

    sloane: build memtrack hidl inplementation

--- device_sloane.mk
+++ device_sloane.mk
@@ -63,6 +63,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	android.hardware.power@1.0-impl
 
+# Memtrack
+PRODUCT_PACKAGES += \
+	android.hardware.memtrack@1.0-impl \
+	android.hardware.memtrack@1.0-service
+
 # Keymaster
 PRODUCT_PACKAGES += \
 	android.hardware.keymaster@3.0-impl
