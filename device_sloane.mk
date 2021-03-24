b0c5611db4b508aa5d5024a18f66d441640e759e
tomascus <arbiter1000@gmail.com>
2021-03-24 21:00:09 +0100

    sloane: build android.hardware.keymaster@3.0-service

--- device_sloane.mk
+++ device_sloane.mk
@@ -70,7 +70,8 @@ PRODUCT_PACKAGES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-	android.hardware.keymaster@3.0-impl
+	android.hardware.keymaster@3.0-impl \
+	android.hardware.keymaster@3.0-service
 
 # Blisrc
 PRODUCT_PACKAGES += \
