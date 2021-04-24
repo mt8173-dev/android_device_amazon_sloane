3512e197d3aa239709be7b23af47a88616cd16a2
Stricted <info@stricted.net>
2021-04-25 01:30:58 +0200

    sloane: power: remove getFeature functionality

--- mediatek/power/power.c
+++ mediatek/power/power.c
@@ -120,13 +120,6 @@ static void set_feature(struct power_module *module __unused,
 {
 }
 
-static int get_feature(struct power_module *module __unused, feature_t feature)
-{
-    if (feature == POWER_FEATURE_SUPPORTED_PROFILES)
-        return POWER_NR_OF_SUPPORTED_PROFILES;
-    return -1;
-}
-
 static struct hw_module_methods_t power_module_methods = {
     .open = NULL,
 };
@@ -145,5 +138,4 @@ struct power_module HAL_MODULE_INFO_SYM = {
     .powerHint = power_hint,
     .setInteractive = power_set_interactive,
     .setFeature = set_feature,
-    .getFeature = get_feature
 };
