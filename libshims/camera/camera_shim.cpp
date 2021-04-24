c69b8ba13b4f3b15ea410f7d50e35faee43847a8
R0rt1z2 <rogerortizleal@gmail.com>
2021-04-25 01:16:02 +0200

    sloane: libshims: camera: extend camera shim
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- libshims/camera/camera_shim.cpp
+++ libshims/camera/camera_shim.cpp
@@ -1,3 +1,10 @@
-extern "C" void _ZN7android19MtkCameraParameters20PIXEL_FORMAT_YUV420IE() {
-	/* sloane has no camera, provide dummy function to satisfy camera libraries */
-}
+/* sloane has no camera, provide dummy functions to satisfy camera libraries */
+extern "C" void _ZN7android19MtkCameraParameters20PIXEL_FORMAT_YUV420IE() {/*N/A*/}
+extern "C" void _ZN7android19MtkCameraParameters19KEY_VR_BUFFER_COUNTE() {/*N/A*/}
+extern "C" void _ZN7android19MtkCameraParameters15KEY_CAMERA_MODEE() {/*N/A*/}
+extern "C" void _ZN7android19MtkCameraParameters18CAMERA_MODE_NORMALE() {/*N/A*/}
+extern "C" void _ZN7android11MtkCamUtils11ImgBufQueueC1EiPKc() {/*N/A*/}
+extern "C" void _ZN7android11MtkCamUtils8FmtUtils18queryImgBufferSizeEPKcjj() {/*N/A*/}
+extern "C" void _ZN7android11MtkCamUtils8FmtUtils19queryImgWidthStrideEPKcjj() {/*N/A*/}
+extern "C" void _ZN7android11MtkCamUtils11DevMetaInfo17queryHalSensorDevEi() {/*N/A*/}
+extern "C" void _ZN7android11MtkCamUtils8FmtUtils15queryPlaneCountEPKc() {/*N/A*/}
