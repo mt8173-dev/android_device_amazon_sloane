292464853192eb8ace9bb6e5c9bf51f0d9304a02
jmpfbmx <jmpf.bmx@gmail.com>
2021-05-12 15:30:45 +0200

    sloane: audio: Switch to passthrough
     * Now audioserver and android.hardware.audio@2.0-service don't crash
    
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- device_sloane.mk
+++ device_sloane.mk
@@ -54,9 +54,7 @@ PRODUCT_PACKAGES += \
 	libnbaio \
 	libeffects \
 	android.hardware.audio@2.0-impl \
-	android.hardware.audio@2.0-service \
-	android.hardware.audio.effect@2.0-impl \
-	android.hardware.audio.effect@2.0-service
+	android.hardware.audio.effect@2.0-impl
 
 # USB
 PRODUCT_PACKAGES += \
