35fc69b5881b24622692f16a2a8b8395233e7bf9
Corinna Vinschen <xda@vinschen.de>
2021-05-15 15:41:47 +0200

    sloane: fix android.hardware.wifi@1.0-service capabilities

--- config.fs
+++ config.fs
@@ -3,3 +3,9 @@ mode: 0700
 user: bluetooth 
 group: bluetooth
 caps: SYS_NICE
+
+[vendor/bin/hw/android.hardware.wifi@1.0-service]
+mode: 0755
+user: AID_WIFI
+group: AID_WIFI
+caps: NET_ADMIN NET_RAW SYS_MODULE
