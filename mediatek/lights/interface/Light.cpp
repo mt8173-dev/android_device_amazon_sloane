2d12f201abb8c52f217bfe3d62aa8039ed5c886b
R0rt1z2 <rogerortizleal@gmail.com>
2021-05-15 22:12:00 +0200

    sloane: lights: 2.0: don't treat light handles as errors
    The MediaTek's light interface tries to open several extra
    lights while some devices may not have these extra lights.
    Treating the bad handle of these extra lights as errors (E)
    may lead to confusion. Instead, switch the logging level to
    warning (W) to avoid further confusion.
    
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- mediatek/lights/interface/Light.cpp
+++ mediatek/lights/interface/Light.cpp
@@ -111,7 +111,7 @@ light_device_t* getLightDevice(const char* name) {
         ret = hwModule->methods->open(hwModule, name,
             reinterpret_cast<hw_device_t**>(&lightDevice));
         if (ret != 0) {
-            ALOGE("light_open %s %s failed: %d", LIGHTS_HARDWARE_MODULE_ID, name, ret);
+            ALOGW("light_open %s %s failed: %d", LIGHTS_HARDWARE_MODULE_ID, name, ret);
         }
     } else {
         ALOGE("hw_get_module %s %s failed: %d", LIGHTS_HARDWARE_MODULE_ID, name, ret);
