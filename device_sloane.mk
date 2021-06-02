2d1c06991dbaf0e94c3facf9451195c49bedc6de
jmpfbmx <jmpf.bmx@gmail.com>
2021-06-02 12:48:52 +0200

    sloane: Move dhcpcd to system
     * It avoids hex-editing dhcpcd binary
     * Also import dhcpcd.conf from stock ROM
    
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- device_sloane.mk
+++ device_sloane.mk
@@ -127,7 +127,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
 	libwpa_client \
 	hostapd \
-	dhcpcd.conf \
 	wpa_supplicant \
 	lib_driver_cmd_mt66xx \
 	wpa_supplicant.conf \
@@ -136,6 +135,7 @@ PRODUCT_PACKAGES += \
 	
 # Wi-Fi Configs
 PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/wifi/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
 	$(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
 	$(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
 	$(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
