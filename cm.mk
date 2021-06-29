89f4940b61d87243a6c5dc5fc16ee313cc2f2bce
R0rt1z2 <rogerortizleal@gmail.com>
2021-06-29 14:53:39 +0200

    sloane: drop useless makefile
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- cm.mk
+++ cm.mk
@@ -1,11 +1,14 @@
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tv.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Include atv base
 $(call inherit-product, device/google/atv/products/atv_base.mk)
 
 # Inherit from sloane device
-$(call inherit-product, device/amazon/sloane/full_sloane.mk)
+$(call inherit-product, device/amazon/sloane/device.mk)
 
 # Bootanimation
 TARGET_SCREEN_WIDTH := 800
