73cd907ab889cf09da809c3d6f4cec792c5e8565
R0rt1z2 <rogerortizleal@gmail.com>
2021-06-29 15:18:56 +0200

    sloane: bluetooth: squash update
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,22 +1,25 @@
-/*
- * Copyright (C) 2014 The CyanogenMod Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
 #define BTM_DEF_LOCAL_NAME   "Amazon Fire TV 2"
+
+#define PROC_BTWRITE_TIMER_TIMEOUT_MS = 0
+
+#define BTA_AV_MAX_A2DP_MTU  1788
+#define BLE_VND_INCLUDED TRUE
+#define BLE_INCLUDED TRUE
+#define BTA_GATT_INCLUDED TRUE
+#define SMP_INCLUDED TRUE
+
+// Turn off BLE_PRIVACY_SPT.  Remote reconnect fails on
+// often if this is enabled.
+#define BLE_PRIVACY_SPT FALSE
+
+// Disable HFP
+#define BTIF_HF_SERVICES (BTA_HSP_SERVICE_MASK)
+#define BTIF_HF_SERVICE_NAMES  { BTIF_HSAG_SERVICE_NAME, NULL }
+
+// To sync with CONN TIMEOUT of pepper
+#define BTM_BLE_CONN_TIMEOUT_DEF 1000
 
 #endif
